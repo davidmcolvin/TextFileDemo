41c95131200342baed9ecce6d0ac0f555aac606c
David M. Colvin <davidmcolvin@gmail.com>
2023-05-23 21:19:03 -0400

    Read from text file and put into class

--- TextFileDemo/ConsoleUI/Program.cs
+++ TextFileDemo/ConsoleUI/Program.cs
@@ -1,13 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleUI;
+
 string filePath = @"C:\Demos\Test.txt";
+
+//List<string> lines = File.ReadAllLines(filePath).ToList();
+
+//foreach (string line in lines)
+//{
+//  Console.WriteLine(line);
+//}
+
+//lines.Add("Sue,Storm,www.stormy.com");
+
+//File.WriteAllLines(filePath, lines);
+
+List<Person> people = new List<Person>();
 
 List<string> lines = File.ReadAllLines(filePath).ToList();
 
-foreach (string line in lines)
+foreach (var line in lines)
 {
-  Console.WriteLine(line);
+  string[] entries = line.Split(',');
+  Person newPerson = new Person();
+
+  newPerson.FirstName = entries[0];
+  newPerson.LastName = entries[1];
+  newPerson.Url = entries[2];
+
+  people.Add(newPerson);
 }
 
-lines.Add("Sue,Storm,www.stormy.com");
-
-File.WriteAllLines(filePath, lines);
+foreach (var person in people)
+{
+  Console.WriteLine($"{person.FirstName} {person.LastName} : {person.Url}");
+}
